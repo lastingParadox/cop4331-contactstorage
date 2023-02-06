3f81a27fa4cb07608a5d1a294a21ea6fd08f33c9
lastingParadox <zanpres@gmail.com>
2023-02-05 22:10:58 -0500

    Add account, settings pages, move navigation-related JS to sidebar.js

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,38 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() 
-{
-    readCookie();
-    searchContacts("");
-
-    let listItems = document.querySelectorAll('.sidebar > ul > li');
-    for (let i = 0; i < listItems.length; i++) {
-        listItems[i].addEventListener('click', function() {
-            document.querySelector(".sidebar > ul > li.active").classList.remove('active');
-            this.classList.add('active');
-        });
-    };
-
-    document.getElementById("hamburger").addEventListener('click', function () {
-        let sidebar = document.getElementById('side_nav')
-        let collapsableElements = document.getElementsByClassName('collapsable');
-
-        if (this.classList.contains('close-btn')) {
-            for (let element of collapsableElements) {
-                element.classList.add("collapse");
-            }
-            sidebar.classList.remove("active");
-            this.classList.remove('close-btn');
-            this.classList.add('open-btn');
-        }
-        else if (this.classList.contains('open-btn')) {
-            sidebar.classList.add("active");
-            for (let element of collapsableElements) {
-                element.classList.remove("collapse");
-            }
-            this.classList.remove('open-btn');
-            this.classList.add('close-btn');
-        }
-    });
-}, false);
+document.addEventListener('DOMContentLoaded', () => searchContacts(""), false);
 
 function editContact() {
 
