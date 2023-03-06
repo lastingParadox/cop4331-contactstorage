842bf5b2c53347346aa394c53034deb84cd0f483
lastingParadox <zanpres@gmail.com>
2023-03-05 21:01:55 -0500

    Reformat php scripts, changes to retrieveUser, remove more unneeded files

--- api/login.php
+++ api/login.php
@@ -3,60 +3,60 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 'on');
 
-	$inData = getRequestInfo();
-	
-	$id = 0;
-	$firstName = "";
-	$lastName = "";
+    $inData = getRequestInfo();
+    
+    $id = 0;
+    $firstName = "";
+    $lastName = "";
 
-	$conn = new mysqli("localhost", "ContactStorage", "contact_storage_pass", "ContactStorageDB"); 	
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		$stmt = $conn->prepare("SELECT id,firstName,lastName,password FROM USERS WHERE username=?");
-		$stmt->bind_param("s", $inData["login"]);
-		$stmt->execute();
-		$result = $stmt->get_result();
+    $conn = new mysqli("localhost", "ContactStorage", "contact_storage_pass", "ContactStorageDB"); 	
+    if( $conn->connect_error )
+    {
+        returnWithError( $conn->connect_error );
+    }
+    else
+    {
+        $stmt = $conn->prepare("SELECT id,firstName,lastName,password FROM USERS WHERE username=?");
+        $stmt->bind_param("s", $inData["login"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
         $row = $result->fetch_assoc();
     
-		if( $row && password_verify($inData['password'], $row['password']) )
-		{
-			returnWithInfo( $row['firstName'], $row['lastName'], $row['id'] );
-		}
-		else
-		{
-			returnWithError("No Records Found");
-		}
+        if( $row && password_verify($inData['password'], $row['password']) )
+        {
+            returnWithInfo( $row['firstName'], $row['lastName'], $row['id'] );
+        }
+        else
+        {
+            returnWithError("No Records Found");
+        }
 
-		$stmt->close();
-		$conn->close();
-	}
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+        $stmt->close();
+        $conn->close();
+    }
+    
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $firstName, $lastName, $id )
-	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":"","success":"Successfully logged in"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+    
+    function returnWithError( $err )
+    {
+        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+    
+    function returnWithInfo( $firstName, $lastName, $id )
+    {
+        $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":"","success":"Successfully logged in"}';
+        sendResultInfoAsJson( $retValue );
+    }
+    
 ?>
